b3e9ccc77ede6d114337a8e69893932feaca789d
unknown <ita384@T440PIE77CL.ITA.GOV.OM>
2014-08-12 14:00:23 +0400

    Jira Task

---
+++ JiRA_Task/src/main/java/jira/test/JiRATest/App.java
@@ -0,0 +1,66 @@
+package jira.test.JiRATest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.atlassian.jira.rest.client.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.domain.Comment;
+import com.atlassian.jira.rest.client.domain.Issue;
+import com.atlassian.jira.rest.client.domain.Transition;
+import com.atlassian.jira.rest.client.domain.input.FieldInput;
+import com.atlassian.jira.rest.client.domain.input.TransitionInput;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+
+/**
+ * Jira Task on GitHub
+ *
+ */
+public class App {
+		
+	public static void main(String[] args) throws URISyntaxException {
+		System.out.println("Starting the code ...");
+		System.out.println("---------------------");
+		
+		final JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+		
+		/*... in production ...*/
+		final URI jiraServerUri = new URI("http://10.26.6.47/");
+		
+		final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "abdullah.alqassabi", "SQU_Sniper57984");
+		
+		final Issue issue = restClient.getIssueClient().getIssue("MO-1233").claim();
+		System.out.println(issue);
+		
+		//restClient.getIssueClient().vote(issue.getVotesUri()).claim();
+		restClient.getIssueClient().watch(issue.getWatchers().getSelf()).claim();
+		
+		// progressing the issue
+		final Iterable<Transition> transitions = restClient.getIssueClient().getTransitions(issue.getTransitionsUri()).claim();
+        final Transition startProgressTransition = getTransitionByName(transitions, "Reopen Issue");
+        restClient.getIssueClient().transition(issue.getTransitionsUri(), new TransitionInput(startProgressTransition.getId())).claim();
+		
+        final Transition resolveIssueTransition = getTransitionByName(transitions, "Resolve Issue");
+        Collection<FieldInput> fieldInputs = Arrays.asList(new FieldInput("resolution", "Incomplete"));
+        final TransitionInput transitionInput = new TransitionInput(resolveIssueTransition.getId(), fieldInputs, Comment.valueOf("My comment"));
+        restClient.getIssueClient().transition(issue.getTransitionsUri(), transitionInput).claim();
+	   	
+        //System.out.println(issue.getTransitionsUri());
+		System.out.println("--------------------");
+	   	System.out.println("... End of the code.");
+	}
+
+	private static Transition getTransitionByName(Iterable<Transition> transitions, String transitionName) 
+	{
+		// TODO Auto-generated method stub
+		for (Transition transition : transitions) {
+            if (transition.getName().equals(transitionName)) {
+            	//System.out.println(transition.getId());
+                return transition;
+            }
+        }
+		return null;
+	}
+}
